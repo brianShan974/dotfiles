e63e3f3ea88e0cae1a7fe2ed1803d5cfe31fc797
brianShan974 <2366910832@qq.com>
2024-03-18 08:00:41 +0000

    Some modifications.

--- latex_neovim/.config/nvim/lua/plugins/luasnip.lua
+++ latex_neovim/.config/nvim/lua/plugins/luasnip.lua
@@ -1,12 +1,12 @@
 return {
 	"L3MON4D3/LuaSnip",
 	build = "make install_jsregexp",
-	dependencies = {
-		"rafamadriz/friendly-snippets",
-		config = function()
-			require("luasnip.loaders.from_vscode").lazy_load()
-		end,
-	},
+	-- dependencies = {
+	-- 	"rafamadriz/friendly-snippets",
+	-- 	config = function()
+	-- 		require("luasnip.loaders.from_vscode").lazy_load()
+	-- 	end,
+	-- },
 	opts = {
 		history = true,
 		delete_check_events = "TextChanged",
@@ -23,7 +23,7 @@ return {
 			"fj",
 			function()
 				return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next"
-					or "<c-\\><c-n>:call searchpair('[([{<|]', '', '[)\\]}>|]', 'W')<cr>a"
+						or "<c-\\><c-n>:call searchpair('[([{<|]', '', '[)\\]}>|]', 'W')<cr>a"
 			end,
 			expr = true,
 			silent = true,
