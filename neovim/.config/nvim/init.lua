c5bbaac8ef4cb329cb3274a7befaaebaa9c872d8
单子亨 <2366910832@qq.com>
2025-01-06 09:23:30 +0000

    Added a new neovim config

---
+++ neovim/.config/nvim/init.lua
@@ -0,0 +1,7 @@
+-- lazy.nvim
+require("config.lazy")
+
+-- configurations
+require("config.options")
+require("config.filetypes")
+require("config.colorscheme")
---
+++ neovim/.config/nvim/lua/config/filetypes.lua
@@ -0,0 +1,1 @@
+vim.api.nvim_command("autocmd BufNew,BufNewFile,BufRead *.vert,*.frag,*tess,*.shader,*.glsl set filetype=glsl")
