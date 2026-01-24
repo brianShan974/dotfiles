989cb9a6d9631af1196dd917988297fb19d48f16
单子亨 <50822674+brianShan974@users.noreply.github.com>
2026-01-24 16:27:42 +0800

    Yazi Config Refactored (#1)
    * feat(yazi): removed all plugins to roll back to a minimal functional yazi
    
    * feat(yazi): added nbpreview.yazi to preview jupyter notebooks
    
    * feat(yazi): added duckdb.yazi to preview data files
    
    * feat(yazi): added starship.yazi to add starship prompt to yazi gui
    
    * feat(yazi): added full-border.yazi for better ui
    
    * feat(yazi): added git.yazi for git information
    
    * style(yazi/keymap.toml): removed useless empty lines
    
    * feat(yazi): added what-size.yazi to check the size of the current directory

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -1,1 +1,14 @@
-require("full-border").setup()
+-- plugin configurations
+-- duckdb.yazi configuration
+require("duckdb"):setup({
+	row_id = "dynamic", -- Default: false
+})
+
+-- starship.yazi configuration
+require("starship"):setup()
+
+-- full-border.yazi configuration
+require("full-border"):setup()
+
+-- git.yazi configuration
+require("git"):setup()
